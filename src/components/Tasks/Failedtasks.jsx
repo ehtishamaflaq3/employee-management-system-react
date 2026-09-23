5a58cd9065094c9480ecb679e31f212ed00d4565
Ehtisham Aflak <ehtishamaflaq12341234@gmail.com>
2026-09-23 08:27:40 +0500

    Update Failedtasks.jsx

--- src/components/Tasks/Failedtasks.jsx
+++ src/components/Tasks/Failedtasks.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Failedtasks = ({data,loggedinUserdata}) => {
+  
   return (
     <div><div className='m-3 h-70 flex-nowrap shrink-0 rounded-2xl justify-between flex flex-col overflow-hidden bg-violet-600 w-80'>
         {/* tasks details card*/}
@@ -9,7 +10,9 @@ const Failedtasks = ({data,loggedinUserdata}) => {
           <h1 className='bg-red-700 rounded-xl h-10 text-amber-200 text-2xl pl-2 pr-2  '>{data.category}</h1>
           <h3 className='text-2xl text-amber-100 underline'>{data.date}</h3>
         </div>
-          {/* task heading and description */}
+      
+      {/* task heading and description */}
+      
           <div className='h-55 w-full flex flex-col justify-between p-3'>
             <h1 className='text-white text-2xl font-bold'>{data.title}</h1>
             <p className='mt-4 text-amber-100 font-bold '>{data.description}</p>
@@ -22,5 +25,4 @@ const Failedtasks = ({data,loggedinUserdata}) => {
         </div></div>
   )
 }
-
 export default Failedtasks
